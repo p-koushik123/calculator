ddbef1f345948859d115775486aa82eda256181e
pkoushik123 <koushikpavani6@gmail.com>
2023-02-28 13:11:41 +0530

    ertyf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom';
+import calculator from './components/calculator';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    
+    <Routes>
+    <calculator title="KL University" aboutText="Aboutus" />
+     
+    </Routes>
+   </>
   );
 }
 
